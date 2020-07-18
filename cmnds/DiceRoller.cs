bc3eab88402fc4099a3650f9596cde6afd1fe4f9
vitalij dmitrievich <rujnikov2015@outlook.com>
2020-07-18 15:40:35 +0800

    added %t param for /r

--- cmnds/DiceRoller.cs
+++ cmnds/DiceRoller.cs
@@ -16,29 +16,22 @@ namespace Dungeon_master.cmnds
 {
     class DiceRoller
     {
-        enum ToDo { 
-            plus,
-            minus,
-            multiply,
-            divide,
-            count
-        }
         [Command("r")]
         [About("Rolls a dice.")]
         public async Task roll(CommandContext cmct, params string[] context) {
             int result = 0;
             int count = 1;
-            //int index = 0;
             int numb = 0;
             int bonus = 0;
+            int total = 0;
+            string message = "";
 
             foreach (string mtch in context) {
 
-                Match m1 = Regex.Match(mtch, @"(\d)?d(\d)(\d)?(\+\d(\d)?)?(\+\d(\d)?)?(\+\d(\d)?)?", RegexOptions.IgnoreCase);
+                Match m1 = Regex.Match(mtch, @"(\d)?d(\d)(\d)?(\+\d(\d)?)?(\+\d(\d)?)?(\+\d(\d)?)?(t)?", RegexOptions.IgnoreCase);
                 string expression = m1.Groups[0].Value;
                 if (m1.Groups[1].Success) {
                     count = int.Parse(expression[0].ToString());
-                    //index = 1;
                 }
                 if (m1.Groups[3].Success)
                     numb = int.Parse(m1.Groups[2].Value + m1.Groups[3].Value);
@@ -47,36 +40,26 @@ namespace Dungeon_master.cmnds
                 for (int i =0;i<count;i++) {
                     result += Commands.r.Next(1,numb+1);
                 }
-                //4 and 5
-                if (m1.Groups[4].Success) {
-                    bonus += int.Parse(expression[m1.Groups[4].Index+1].ToString());
-                    if (m1.Groups[5].Success) {
-                        bonus += int.Parse(expression[m1.Groups[4].Index + 1].ToString()+ expression[m1.Groups[5].Index].ToString())-1;
-                    }
-                }
-                //6 and 7
-                if (m1.Groups[6].Success)
-                {
-                    bonus += int.Parse(expression[m1.Groups[6].Index + 1].ToString());
-                    if (m1.Groups[7].Success)
+                for (int i =4;i<9;i=i+2) {
+                    if (m1.Groups[i].Success)
                     {
-                        bonus += int.Parse(expression[m1.Groups[6].Index + 1].ToString() + expression[m1.Groups[7].Index].ToString())-1;
-                    }
-                }
-                //8 and 9
-                if (m1.Groups[8].Success)
-                {
-                    bonus += int.Parse(expression[m1.Groups[8].Index + 1].ToString());
-                    if (m1.Groups[9].Success)
-                    {
-                        bonus += int.Parse(expression[m1.Groups[8].Index + 1].ToString() + expression[m1.Groups[9].Index].ToString())-1;
+                        bonus += int.Parse(expression[m1.Groups[i].Index + 1].ToString());
+                        if (m1.Groups[i+1].Success)
+                        {
+                            bonus += int.Parse(expression[m1.Groups[i].Index + 1].ToString() + expression[m1.Groups[i+1].Index].ToString()) - 1;
+                        }
                     }
                 }
                 result += bonus;
-                await cmct.RespondAsync("> "+expression+" = "+result).ConfigureAwait(false);
+                if (m1.Groups[10].Success) total +=result;
+                message += "> " + expression + " = " + result+"\n";
+                //await cmct.RespondAsync("> "+expression+" = "+result).ConfigureAwait(false);
                 result = 0;
                 bonus = 0;
             }
+            await cmct.RespondAsync(message);
+            if(total!=0) await cmct.RespondAsync("> total = " + total).ConfigureAwait(false);
+            total = 0;
         }
     }
 }
