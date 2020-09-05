1dfd392fe28cd7d518b6e6504a4c107acb275886
Witalij Ruzhnikov <rujnikov2015@outlook.com>
2020-09-05 17:47:09 +0800

    added DB for random tables

--- dungeon generator/Builders.cs
+++ dungeon generator/Builders.cs
@@ -1,21 +1,29 @@
 using System;
 
 namespace Dungeon_master{
-    abstract class RoomBuilder{
-        Room room = new Room();
-        randomTable rt = new randomTable();
-        public abstract Room build();
+    abstract class dungeonType{
+        public string path {get;set;}
     }
-    class planarRoomBuilder : RoomBuilder{
+    class RoomBuilder{
         Room room = new Room();
         randomTable rt = new randomTable();
-        public override Room build(){
-            room.destination = rt.Generate("dungeon generator/donj_planar.txt",101);
+        dungeonType dt;
+        public RoomBuilder(dungeonType dt){
+            this.dt=dt;
+        }
+        public Room build(){
+            room.destination = rt.Generate(dt.path,101);
             room.Form = rt.Generate("dungeon generator/room_form.txt",21);
-            room.exits = Commands.r.Next(0,6);
+            room.exits = Commands.r.Next(1,6);
             room.state = rt.Generate("dungeon generator/room_state.txt",21);
-            room.smell = rt.Generate("dungeon generator/room_smell.txt",100);
+            room.smell = rt.Generate("dungeon generator/room_smell.txt",101);
+            room.noise = rt.Generate("dungeon generator/room_noise.txt",101);
+            room.air = rt.Generate("dungeon generator/room_air.txt",101);
             return room;
         }
     }
+    class planarDungeon : dungeonType{
+        public string path = "dungeon generator/donj_planar.txt";
+    }
+    
 }
